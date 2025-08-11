a5e35081f46db09fca8d50ad623c3130646277e9
Steve Kang <steve.kang96@gmail.com>
2025-08-11 06:24:02 -0700

    Add PredictorPage placeholder

---
+++ apps/web/src/pages/PredictorPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PredictorPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Predictor</h1>
+      <p>This is a temporary placeholder page. Coming soon.</p>
+    </div>
+  );
+}
